236f0a9bcc9fc260890f54092533c322a8210990
jihoi0615 <pjhalice0615@gmail.com>
2025-03-13 14:49:34 +0900

    FEAT: project init

---
+++ src/test/java/com/uplus/raindrop/RaindropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplus.raindrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaindropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
