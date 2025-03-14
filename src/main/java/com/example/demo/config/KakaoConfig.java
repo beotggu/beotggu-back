3de4379ae295e1001d2bd6057fa9e028094e995a
silverain <ekdldkaa@naver.com>
2025-03-14 16:53:32 +0900

    #3 fix : cofig setter annotation 추가

--- src/main/java/com/example/demo/config/KakaoConfig.java
+++ src/main/java/com/example/demo/config/KakaoConfig.java
@@ -1,12 +1,14 @@
 package com.example.demo.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "kakao")
 @Getter
+@Setter
 public class KakaoConfig {
     private String clientId;
     private String redirectUri;
